67a6ffed8592d4c8503a08eb1cd89d5675e4d539
yabwang <1045875623@qq.com>
2025-06-16 10:45:43 +0800

    fix

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -24,6 +24,12 @@ export default defineConfig({
             { text: '数据库', link: '/basic/mysql' },
           ],
         }
+        // ,{
+        //   text: 'java 基础',
+        //   items: [
+        //     { text: 'java 基础', link: '/java/' },
+        //   ],
+        // }
 
       ],
 
@@ -35,7 +41,7 @@ export default defineConfig({
             collapsed: true,
             items: [
               { text: 'LeetCode 100题', link: '/algorithms/leetCode100' },
-              { text: '动态规划', link: '/algorithms/dp' },
+              // { text: '动态规划', link: '/algorithms/dp' },
               { text: '并查集', link: '/algorithms/unionFind' }
               // ... existing code ...
             ]
